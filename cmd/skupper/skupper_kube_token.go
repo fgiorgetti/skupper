0657914fb866124b85fce1de1408e445981de088
Fernando Giorgetti <fgiorget@redhat.com>
2022-09-29 13:50:24 -0300

    Token create CLI implementation for podman

--- cmd/skupper/skupper_kube_token.go
+++ cmd/skupper/skupper_kube_token.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/skupperproject/skupper/api/types"
 	"github.com/skupperproject/skupper/pkg/utils"
@@ -49,4 +50,9 @@ func (s *SkupperKubeToken) Create(cmd *cobra.Command, args []string) error {
 	}
 }
 
-func (s *SkupperKubeToken) CreateFlags(cmd *cobra.Command) {}
+func (s *SkupperKubeToken) CreateFlags(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&tokenType, "token-type", "t", "claim", "Type of token to create. Valid options are 'claim' or 'cert'")
+	cmd.Flags().StringVarP(&password, "password", "p", "", "A password for the claim (only valid if --token-type=claim). If not specified one will be generated.")
+	cmd.Flags().DurationVarP(&expiry, "expiry", "", 15*time.Minute, "Expiration time for claim (only valid if --token-type=claim)")
+	cmd.Flags().IntVarP(&uses, "uses", "", 1, "Number of uses for which claim will be valid (only valid if --token-type=claim)")
+}
