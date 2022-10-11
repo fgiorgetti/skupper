b45402949f0040e73c55bfb13acf43b99cdf01b6
Fernando Giorgetti <fgiorget@redhat.com>
2022-10-11 15:50:47 -0300

    Removed the site query skip condition temporarily

--- cmd/service-controller/site_query.go
+++ cmd/service-controller/site_query.go
@@ -343,9 +343,9 @@ func querySites(agent qdr.RequestResponse, sites []data.SiteQueryData) {
 		// Non kubernetes sites do not yet have a Service Controller,
 		// this request won't be sent to avoid an unnecessary timeout.
 		// TODO Remove this once non-kubernetes have a service controller in place
-		if s.Namespace == "" {
-			continue
-		}
+		// if s.Namespace == "" {
+		// 	continue
+		// }
 		response, err := agent.Request(&request)
 		if err != nil {
 			event.Recordf(SiteQueryError, "Request to %s failed: %s", s.SiteId, err)
@@ -389,9 +389,9 @@ func queryGateways(agent qdr.RequestResponse, sites []data.SiteQueryData) []data
 		// Non kubernetes sites do not yet have a Service Controller,
 		// this request won't be sent to avoid an unnecessary timeout.
 		// TODO Remove this once non-kubernetes have a service controller in place
-		if s.Namespace == "" {
-			continue
-		}
+		// if s.Namespace == "" {
+		// 	continue
+		// }
 		response, err := agent.Request(&request)
 		if err != nil {
 			event.Recordf(GatewayQueryError, "Request to %s failed: %s", s.SiteId, err)
