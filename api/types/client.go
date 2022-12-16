b9aa80ce10b3c2712b73ea5ef2f22707274262d6
Fernando Giorgetti <fgiorget@redhat.com>
2022-12-16 16:14:02 -0300

    Defined ingress types as external and none for podman sites

--- api/types/client.go
+++ api/types/client.go
@@ -111,7 +111,7 @@ const (
 	IngressNginxIngressString     string = "nginx-ingress-v1"
 	IngressContourHttpProxyString string = "contour-http-proxy"
 	IngressKubernetes             string = "ingress"
-	IngressPodmanHost             string = "host"
+	IngressPodmanExternal         string = "external"
 	IngressNoneString             string = "none"
 )
 
@@ -134,7 +134,7 @@ func (s *SiteConfigSpec) IsIngressKubernetes() bool {
 	return s.Ingress == IngressKubernetes
 }
 func (s *SiteConfigSpec) IsIngressPodmanHost() bool {
-	return s.Ingress == IngressPodmanHost
+	return s.Ingress == IngressPodmanExternal
 }
 func (s *SiteConfigSpec) IsIngressNone() bool {
 	return s.Ingress == IngressNoneString
@@ -171,7 +171,7 @@ func (s *SiteConfigSpec) getConsoleIngress() string {
 func ValidIngressOptions(platform Platform) []string {
 	switch platform {
 	case PlatformPodman:
-		return []string{IngressPodmanHost}
+		return []string{IngressPodmanExternal, IngressNoneString}
 	default:
 		return []string{IngressRouteString, IngressLoadBalancerString, IngressNodePortString, IngressNginxIngressString, IngressContourHttpProxyString, IngressKubernetes, IngressNoneString}
 	}
