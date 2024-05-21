3b57bf1c13a0bca9af09ffbad9eb643354a7a55f
Fernando Giorgetti <fgiorget@redhat.com>
2024-05-21 17:21:15 -0300

    Updated temporary remove script

--- cmd/bootstrap/remove.sh
+++ cmd/bootstrap/remove.sh
@@ -1,11 +1,19 @@
 set -Ceu
 
 site=$1
+sites_path="${HOME}/.local/share/skupper/sites"
+service_path="${HOME}/.config/systemd/user"
+systemctl="systemctl --user"
+if [[ ${UID} -eq 0 ]]; then
+    sites_path="/usr/local/share/skupper/sites"
+    service_path="/etc/systemd/system"
+    systemctl="systemctl"
+fi
 podman rm -f ${site}-skupper-router
-rm -rf ~/.local/share/skupper/sites/${site}
+rm -rf ${sites_path}/${site}/
 service="skupper-site-${site}.service"
-systemctl --user stop ${service}
-systemctl --user disable ${service}
-rm -f ~/.config/systemd/user/${service}
-systemctl --user daemon-reload
-systemctl --user reset-failed
+${systemctl} stop ${service}
+${systemctl} disable ${service}
+rm -f ${service_path}/${service}
+${systemctl} daemon-reload
+${systemctl} reset-failed
