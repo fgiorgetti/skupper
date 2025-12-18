baa7880cd9ab4d7f1aa6443f06f69cf05840d6ca
Fernando Giorgetti <fgiorget@redhat.com>
2025-12-18 12:45:50 -0300

    Initial handling of NetworkAccess
    # Conflicts:
    #	internal/kube/site/site.go

--- internal/qdr/qdr.go
+++ internal/qdr/qdr.go
@@ -1227,6 +1227,13 @@ func IsNotProtectedListener(l Listener) bool {
 	return true
 }
 
+func IsInterNetworkNotProtectedListener(l Listener) bool {
+	if !IsNotProtectedListener(l) {
+		return false
+	}
+	return l.Role == RoleInterNetwork
+}
+
 func FilterListeners(in map[string]Listener, predicate ListenerPredicate) map[string]Listener {
 	results := map[string]Listener{}
 	for key, listener := range in {
