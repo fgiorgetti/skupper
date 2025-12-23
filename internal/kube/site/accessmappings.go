6bfa245e5a258c57daf823195d375053cb325495
Fernando Giorgetti <fgiorget@redhat.com>
2025-12-23 16:17:54 -0300

    Reusing code between RouterAccess and NetworkAccess

--- internal/kube/site/accessmappings.go
+++ internal/kube/site/accessmappings.go
@@ -1,28 +1,15 @@
 package site
 
-type securedAccessMapping struct {
-	RouterAccessName string
-	Group            string
+type accessMapping struct {
+	AccessName string
+	Group      string
 }
 
-type networkAccessMapping struct {
-	NetworkAccessName string
-	Group             string
-}
-
-func newSecuredAccessMapping(routerAccess, group string) securedAccessMapping {
-	return securedAccessMapping{
-		RouterAccessName: routerAccess,
-		Group:            group,
+func newAccessMapping(accessName, group string) accessMapping {
+	return accessMapping{
+		AccessName: accessName,
+		Group:      group,
 	}
 }
 
-func newNetworkAccessMapping(networkAccess, group string) networkAccessMapping {
-	return networkAccessMapping{
-		NetworkAccessName: networkAccess,
-		Group:             group,
-	}
-}
-
-type securedAccessMap map[string]securedAccessMapping
-type networkAccessMap map[string]networkAccessMapping
+type accessMap map[string]accessMapping
