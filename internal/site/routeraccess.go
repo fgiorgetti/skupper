74d8445d4e510e94ff5610010ed02d1a50b8bd40
Fernando Giorgetti <fgiorget@redhat.com>
2026-04-06 17:26:37 -0300

    Allow deleting Normal listeners on kube sites

--- internal/site/routeraccess.go
+++ internal/site/routeraccess.go
@@ -75,7 +75,7 @@ type RouterAccessConfig struct {
 
 func (g *RouterAccessConfig) Apply(config *qdr.RouterConfig) bool {
 	changed := false
-	lc := qdr.ListenersDifference(config.GetMatchingListeners(qdr.IsInterRouterEdgeNotProtectedListener), g.listeners)
+	lc := qdr.ListenersDifference(config.GetMatchingListeners(qdr.IsSupportedAndNotProtectedListener), g.listeners)
 	// delete before add with listeners, as changes are handled as delete and add
 	for _, value := range lc.Deleted {
 		if removed, _ := config.RemoveListener(value.Name); removed {
